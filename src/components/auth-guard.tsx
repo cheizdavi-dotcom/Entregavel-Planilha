d71e2c77da33985be02a6636699a4268142c2d6b
Davi Cheiz <davicheizz9@gmail.com>
2025-11-27 19:42:48 +0000

    continua o bug do fantasma ali, arruma defiinitivo

--- src/components/auth-guard.tsx
+++ src/components/auth-guard.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/hooks/use-auth';
 import { useEffect } from 'react';
-import { Skeleton } from '@/components/ui/skeleton';
+import Loading from '@/app/loading';
 
 const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const { user, loading } = useAuth();
@@ -16,26 +16,7 @@ const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   }, [user, loading, router]);
 
   if (loading || !user) {
-    return (
-        <div className="flex flex-col min-h-screen bg-background">
-            <div className="flex-1 space-y-8 p-4 md:p-8 pt-6">
-                <div className="flex items-center justify-between space-y-2">
-                    <Skeleton className="h-8 w-48" />
-                    <Skeleton className="h-8 w-20" />
-                </div>
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                    <Skeleton className="h-[126px] rounded-lg" />
-                    <Skeleton className="h-[126px] rounded-lg" />
-                    <Skeleton className="h-[126px] rounded-lg" />
-                </div>
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7">
-                    <Skeleton className="h-[350px] lg:col-span-4 rounded-lg" />
-                    <Skeleton className="h-[350px] lg:col-span-3 rounded-lg" />
-                </div>
-                <Skeleton className="h-[300px] rounded-lg" />
-            </div>
-      </div>
-    );
+    return <Loading />;
   }
 
   return <>{children}</>;
