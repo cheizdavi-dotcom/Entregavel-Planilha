8c1f3394949d4fc4d31b18bd38f949c41c06260c
Davi Cheiz <davicheizz9@gmail.com>
2025-12-31 20:31:33 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,11 +1,11 @@
 // COLE SUAS CHAVES DO FIREBASE AQUI
 // Este objeto 'firebaseConfig' é o local correto para suas chaves.
 export const firebaseConfig = {
-  "projectId": "neonwallet-c22a1",
-  "appId": "1:655428450372:web:2530b127598c9e5e7b5f13",
-  "storageBucket": "neonwallet-c22a1.appspot.com",
   "apiKey": "AIzaSyChTfB-fB-c8G7yA1B6S1E3G6A3J5b7M8",
   "authDomain": "neonwallet-c22a1.firebaseapp.com",
+  "projectId": "neonwallet-c22a1",
+  "storageBucket": "neonwallet-c22a1.appspot.com",
   "messagingSenderId": "655428450372",
+  "appId": "1:655428450372:web:2530b127598c9e5e7b5f13",
   "measurementId": "G-99T8J8E8R8"
 };
