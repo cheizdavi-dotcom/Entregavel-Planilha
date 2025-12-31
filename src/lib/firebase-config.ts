7feed133ef868a05045bdff58736ec2f549000a3
Davi Cheiz <davicheizz9@gmail.com>
2025-12-31 20:53:45 +0000

    to aqui, outra coisa, tem esse codigo q peguei nas configuraçoes do proj

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,11 +1,11 @@
 // ESTE ARQUIVO FOI ATUALIZADO COM A CONFIGURAÇÃO CORRETA DO SEU PROJETO FIREBASE.
 // O erro "auth/api-key-not-valid" foi resolvido.
 export const firebaseConfig = {
-  "apiKey": "AIzaSyChTfB-fB-c8G7yA1B6S1E3G6A3J5b7M8",
-  "authDomain": "neonwallet-c22a1.firebaseapp.com",
-  "projectId": "neonwallet-c22a1",
-  "storageBucket": "neonwallet-c22a1.appspot.com",
-  "messagingSenderId": "655428450372",
-  "appId": "1:655428450372:web:2530b127598c9e5e7b5f13",
-  "measurementId": "G-99T8J8E8R8"
+  apiKey: "AIzaSyBPd6NvwuDnlkT0rOVeIrTpO5m3jezszoo",
+  authDomain: "studio-7230625454-2a5c2.firebaseapp.com",
+  projectId: "studio-7230625454-2a5c2",
+  storageBucket: "studio-7230625454-2a5c2.appspot.com",
+  messagingSenderId: "908240042642",
+  appId: "1:908240042642:web:1b4cd5d5eb1d1ffe17c44b",
+  measurementId: "G-74Q8D5KS9M"
 };
